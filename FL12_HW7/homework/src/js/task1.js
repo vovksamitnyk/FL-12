7aae701572706bd995a17e4ff0b6a724307a74cc
Dmytro Yatsyshyn <vovksamitnyk@gmail.com>
2019-12-27 00:02:18 +0200

    Add FL12_HW7

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -1,1 +1,70 @@
-// Your code goes here
+const user01 = {email: 'user@gmail.com', password: 'UserPass'};
+const user02 = {email: 'admin@gmail.com', password: 'AdminPass'};
+const minEmailLength = 5;
+const minPassLength = 6;
+
+let users = [user01, user02];
+
+let userFound = false;
+let wrongEnter = false;
+let tooShortPass = false;
+
+let enteredEmail = prompt('Enter email:');
+if (enteredEmail) {
+   if (enteredEmail.length < minEmailLength) {
+      alert('I don\'t know any emails having name ' +
+            'length less than ' + minEmailLength + ' symbols');
+      tooShortPass = true;
+   }
+   for (let i = 0; i < users.length; i++) {
+      if (enteredEmail === users[i].email) {
+         let enteredPassword = prompt('Enter password:');
+         if (enteredPassword === null) {
+            alert('Canceled.');
+            tooShortPass = true;
+         } else if (enteredPassword === users[i].password) {
+            userFound = true;
+            if (confirm('Do you want to change your password?')) {
+               let enteredOldPassword = prompt('Enter Your old password:');
+               if (enteredOldPassword === users[i].password) {
+                  let enteredNewPassword = prompt('Enter Your new password:');
+                  if (enteredNewPassword === null) {
+                     alert('Canceled.');
+                     break
+                  } else if (enteredNewPassword.length < minPassLength) {
+                     alert('It’s too short password. Sorry.');
+                  } else {
+                     let enteredNewPassword2 = prompt('Enter Your new password again:');
+                     if (enteredNewPassword2 === null) {
+                        alert('Canceled.');
+                        break
+                     } else if (enteredNewPassword2 !== enteredNewPassword) {
+                        alert('You wrote the wrong password.');
+                     } else {
+                        users[i].password = enteredNewPassword;
+                        alert('You have successfully changed your password.');
+                     }
+                  }
+               } else {
+                  alert('Wrong password!');
+               }
+            } else {
+               alert('You have failed the change.');
+            }
+         } else {
+            alert('Wrong password!');
+            tooShortPass = true;
+         }
+         break
+      }
+   }
+   if (!userFound && !tooShortPass) {
+      alert('I don’t know you.');
+   }
+} else {
+   wrongEnter = true;
+}
+
+if (wrongEnter) {
+   alert('Canceled.');
+}
--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,1 +1,85 @@
-// Your code goes here
+let start = confirm('Do you want to play a game?');
+const rangeIncrease = 4;
+const prizeIncrease = 2;
+const prizeDecrease = 2;
+const maxAttempts = 3;
+
+if (start) {
+   let game = true;
+   while (game) {
+      let topPossiblePrize = 100;
+      let topLimitNumber = 8;
+      let totalPrize = 0;
+      let possiblePrize = 100;
+
+      let randomNumber = Math.floor(Math.random() * (topLimitNumber + 1));
+      console.log(randomNumber);
+
+      let attempt = maxAttempts;
+      let enterGues;
+
+      while (attempt > 0 && enterGues !== randomNumber) {
+         if (attempt < maxAttempts) {
+            possiblePrize = possiblePrize / prizeDecrease;
+         }
+         enterGues = parseInt(prompt(
+            'Choose a roulette pocket number from 0 to ' + topLimitNumber +
+            '\nAttempts left: ' + attempt +
+            '\nTotal prize: ' + totalPrize + '$' +
+            '\nPossible prize on current attempt: ' + possiblePrize + '$'));
+         attempt--;
+      }
+
+      if (enterGues === randomNumber) {
+         totalPrize += possiblePrize;
+         let continueGame = confirm('Congratulation, you won!   ' +
+                                    'Your prize is: ' + totalPrize + ' $. Do you want to continue?');
+         if (continueGame) {
+            topLimitNumber += rangeIncrease;
+            topPossiblePrize *= prizeIncrease;
+            possiblePrize = topPossiblePrize;
+            enterGues = undefined;
+            randomNumber = Math.floor(Math.random() * (topLimitNumber + 1));
+            console.log(randomNumber);
+
+            while (continueGame) {
+               attempt = maxAttempts;
+               enterGues = undefined;
+               while (attempt > 0 && enterGues !== randomNumber) {
+                  if (attempt < maxAttempts) {
+                     possiblePrize = possiblePrize / prizeDecrease;
+                  }
+                  enterGues = parseInt(prompt('Choose a roulette pocket number from 0 to ' + topLimitNumber +
+                                              '\nAttempts left: ' + attempt +
+                                              '\nTotal prize: ' + totalPrize + '$' +
+                                              '\nPossible prize on current attempt: ' + possiblePrize + '$'));
+                  attempt--;
+               }
+
+               if (enterGues === randomNumber) {
+                  totalPrize += possiblePrize;
+                  continueGame = confirm('Congratulation, you won!   ' +
+                                         'Your prize is: ' + totalPrize + '$. Do you want to continue?');
+                  if (continueGame) {
+                     topLimitNumber += rangeIncrease;
+                     topPossiblePrize *= prizeIncrease;
+                     possiblePrize = topPossiblePrize;
+                     randomNumber = Math.floor(Math.random() * (topLimitNumber + 1));
+                     console.log(randomNumber);
+
+                     enterGues = undefined;
+                  }
+               } else {
+                  totalPrize = 0;
+                  continueGame = false;
+               }
+            }
+         }
+      }
+
+      alert('Thank you for your participation. Your prize is: ' + totalPrize + '$');
+      game = confirm('Do you want play again?');
+
+   }
+}
+alert('You did not become a billionaire, but can.');
